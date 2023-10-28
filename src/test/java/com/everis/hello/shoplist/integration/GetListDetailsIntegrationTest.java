8d5f272f36747dec9224e7b6368f2af08be5271a
EnricRG <u1939673@campus.udg.edu>
2023-10-28 23:09:38 +0200

    Product removal on list detail integration test.

--- src/test/java/com/everis/hello/shoplist/integration/GetListDetailsIntegrationTest.java
+++ src/test/java/com/everis/hello/shoplist/integration/GetListDetailsIntegrationTest.java
@@ -37,6 +37,7 @@ class GetListDetailsIntegrationTest {
         assertTrue(ShopListTestUtils.shopListExists(shopListJpaRepo, user, listName));
         ShopListEntity list = shopListJpaRepo.findByOwnerAndListName(user, listName).orElseThrow();
         int initialProductNumber = list.getItems().size();
+
         ResponseEntity<DetailedShopListView> response = controller.getListDetails(user, listName);
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
@@ -51,5 +52,36 @@ class GetListDetailsIntegrationTest {
         for (ShopListItem dbItem : list.getItems()) {
             assertTrue(detailView.getProducts().stream().anyMatch(x -> x.getId().equals(dbItem.getProductId())));
         }
+    }
+
+    @Test
+    @Transactional
+    void getDetailsForExistingListWithUnknownProducts_shouldReturnOkWithSomeProductRemovals() throws ShopListNotFoundException {
+        String user = "userWithFullShopList";
+        String listName = "fullShopList"; // List with 25 elements ranging from 1 to 25, some do not exist in the product service.
+
+        assertTrue(ShopListTestUtils.shopListExists(shopListJpaRepo, user, listName));
+        ShopListEntity list = shopListJpaRepo.findByOwnerAndListName(user, listName).orElseThrow();
+        int initialProductNumber = list.getItems().size();
+
+        ResponseEntity<DetailedShopListView> response = controller.getListDetails(user, listName);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+
+        list = shopListJpaRepo.findByOwnerAndListName(user, listName).orElseThrow();
+        assertTrue(initialProductNumber > list.getItems().size()); // Check that some products have been deleted
+
+        DetailedShopListView detailView = response.getBody();
+        assertEquals(detailView.getProducts().size(), list.getItems().size());
+        // Check that all items stored in db are represented in the view.
+        for (ShopListItem dbItem : list.getItems()) {
+            assertTrue(detailView.getProducts().stream().anyMatch(x -> x.getId().equals(dbItem.getProductId())));
+        }
+
+        // We know mock service contains products ranging from IDs [1,15] and the list contained IDs up to 25
+        // so 10 products should have been deleted.
+        assertEquals(15, list.getItems().size());
+        assertTrue(list.getItems().stream().noneMatch(x -> x.getProductId() > 15));
     }
 }
